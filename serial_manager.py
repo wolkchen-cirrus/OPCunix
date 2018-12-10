05277cad8f9f30ef8a3e474b326b0f6a833d1de7
JGirdwood <j.girdwood@herts.ac.uk>
2018-12-10 16:29:16 +0000

    Merge branch 'master' of https://github.com/JGirdwood/OPCunix
    Conflicts:
    	README.md

---
+++ serial_manager.py
@@ -0,0 +1,25 @@
+import serial
+import glob
+
+def list_ports():
+    """ Gets Serial Port Names (**nix only)
+        :returns:
+            A list of serial ports currently available
+    """
+    ports = glob.glob('/dev/tty[A-Za-Z]*')
+    ports_out = []
+    for i in ports:
+        try:
+            port = serial.Serial(i)
+            port.close()
+            ports_out.append(i)
+        except (OSError, serial.SerialException)
+            pass
+    return ports_out
+
+
+
+
+
+
+
