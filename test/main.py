8ed332418d9a48526bfde7857d53606348025b8e
JGirdwood <j.girdwood@herts.ac.uk>
2019-02-15 16:14:05 +0000

    Updated Example

--- test/main.py
+++ test/main.py
@@ -1,9 +1,9 @@
-import OPCunix.command_handler
+import OPCunix
 
 
 OPCunix.command_handler.make_title()
 while True:
-    command = raw_input("[OPC-unix] ")
+    command = raw_input("[OPC-unix] >> ")
     c = OPCunix.command_handler.process(command)
     if c == 0:
         break
