45d072c2e749446a5f3a9e970b5bca2e94cf6050
JGirdwood <j.girdwood@herts.ac.uk>
2019-02-22 11:19:15 +0000

    Added setup.py for release

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="UH-OPCunix-JGirdwood",
+    version="0.0.22",
+    author="Joseph Girdwood",
+    author_email="j.girdwood@herts.ac.uk",
+    description="A package to interface a UCASS with a linux computer via USB-ISS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JGirdwood/OPCunix",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+)
