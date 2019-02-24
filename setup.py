d9ce4c8a9335406dc0d726a5888356485044f13a
Joseph Girdwood <j.girdwood@herts.ac.uk>
2019-02-24 22:38:09 +0000

    Added Dependencies

--- setup.py
+++ setup.py
@@ -18,4 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
     ],
+    install_requires=[
+        'pyserial',
+        'pyusbiss'
+    ],
 )
